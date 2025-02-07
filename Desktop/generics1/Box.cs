57b4222cbdd276d320728b3d30cac4cdca275938
sinanwaseem2008 <sinanwaseem23@gmail.com>
2025-02-07 20:10:13 +0500

    initial commit

---
+++ Desktop/generics1/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics1
+{
+    internal class Box<T>
+    {
+        private T _value;
+
+        public void SetValue(T value)
+        {
+            _value = value;
+        }
+        public T GetValue()
+        {
+            return _value;
+        }
+    }
+}
---
+++ Desktop/generics1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Intvalue = new Box<int>();
+            Intvalue.SetValue(95);
+
+            Console.WriteLine(Intvalue.GetValue());
+
+            var Stringvalue = new Box<string>();
+            Stringvalue.SetValue("Hello World");
+            Console.WriteLine(Stringvalue.GetValue());
+
+            List<string> ListString = new List<string>(); 
+            ListString.Add("Maaz");
+            ListString.Add("Shayan");
+            ListString.Add("Qadir");
+            ListString.Add("Hasnain");
+
+            Console.WriteLine(ListString[0]);
+
+
+
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
